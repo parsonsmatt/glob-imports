806fdaf59767f8b10d692d7f2a993caed110a157
Daniel Powell <dtpowl@gmail.com>
2025-05-20 12:24:55 -0600

    fourmolu.yaml

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,34 @@
 module Main where
 
 import GlobImports.Exe
---import Control.Applicative (optional)
 import Options.Applicative
 import System.IO (readFile')
 
 data Args = Args
-  { argsOriginalFile :: FilePath
-  , argsSourceFile :: FilePath
-  , argsDest :: FilePath
-  , argsSearchDir :: Maybe FilePath
-  , argsPattern :: String
-  , argsExcludedPrefixes :: String
-  , argsDebug :: Bool
-  } deriving (Show)
+    { argsOriginalFile :: FilePath
+    , argsSourceFile :: FilePath
+    , argsDest :: FilePath
+    , argsSearchDir :: Maybe FilePath
+    , argsPattern :: String
+    , argsExcludedPrefixes :: String
+    , argsDebug :: Bool
+    }
+    deriving (Show)
 
 parseArgs :: IO Args
 parseArgs =
-  execParser $
-  info (Args
-        <$> originalFileParser
-        <*> sourceFileParser
-        <*> destFileParser
-        <*> searchDirParser
-        <*> patternParser
-        <*> excludedPrefixesParser
-        <*> debugParser) mempty
+    execParser $
+        info
+            ( Args
+                <$> originalFileParser
+                <*> sourceFileParser
+                <*> destFileParser
+                <*> searchDirParser
+                <*> patternParser
+                <*> excludedPrefixesParser
+                <*> debugParser
+            )
+            mempty
   where
     originalFileParser = argument str (metavar "ORIGINAL_FILE")
     sourceFileParser = argument str (metavar "SOURCE_FILE")
@@ -33,24 +36,24 @@ parseArgs =
     searchDirParser =
         optional . (option str) $
             long "search-dir"
-            <> metavar "SEARCH_DIR"
-            <> help "Directory to search for files in"
+                <> metavar "SEARCH_DIR"
+                <> help "Directory to search for files in"
     patternParser =
         option str $
             long "pattern"
-            <> metavar "PATTERN"
-            <> help "Pattern on which to match file paths"
-            <> value "**/*.hs"
+                <> metavar "PATTERN"
+                <> help "Pattern on which to match file paths"
+                <> value "**/*.hs"
     excludedPrefixesParser =
         option str $
             long "exclude-prefixes"
-            <> metavar "PREFIXES"
-            <> help "File path prefixes to exclude"
-            <> value ""
+                <> metavar "PREFIXES"
+                <> help "File path prefixes to exclude"
+                <> value ""
     debugParser =
         switch $
             long "debug"
-            <> help "Whether to print debug output"
+                <> help "Whether to print debug output"
 
 main :: IO ()
 main = do
@@ -64,7 +67,7 @@ main = do
         (argsPattern args)
         (splitOn ',' (argsExcludedPrefixes args))
         (argsDebug args)
-    where
-      splitOn _ [] = []
-      splitOn sep xs =
+  where
+    splitOn _ [] = []
+    splitOn sep xs =
         takeWhile (/= sep) xs : splitOn sep (drop 1 $ dropWhile (/= sep) xs)
