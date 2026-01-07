8cef37b1679781784a9fa1a3002ee2b220610c69
Harry Garrood <harry@garrood.me>
2026-01-07 16:48:43 +0000

    Make output deterministic (#8)
    * Sort `find` output
    
    * Bump version + changelog

--- src/GlobImports/Exe.hs
+++ src/GlobImports/Exe.hs
@@ -160,7 +160,7 @@ spliceImports (Source src) (SourceContents srcContents) (Destination dest) msear
       simplifyPath :: String -> String
       simplifyPath = (intercalate "/") . (filter (/= mempty)) . (splitOn "/")
 
--- | Returns a list of relative paths to all files in the given directory.
+-- | Returns a sorted list of relative paths to all files in the given directory.
 getFiles
     :: FilePath
     -- ^ The glob pattern to filter with.
@@ -170,7 +170,7 @@ getFiles
 getFiles baseDir pat = do
     (exitCode, out, err) <- (readProcess $ proc "find" [baseDir, "-wholename", pat]) `catch` handler
     pure $ case exitCode of
-        ExitSuccess -> Right . lines . Text.unpack . decodeUtf8 . LBS.toStrict $ out
+        ExitSuccess -> Right . sort . lines . Text.unpack . decodeUtf8 . LBS.toStrict $ out
         ExitFailure _ -> Left . Text.unpack . decodeUtf8 . LBS.toStrict $ err
     where
       handler :: SomeException -> IO (ExitCode, LBS.ByteString, LBS.ByteString)
